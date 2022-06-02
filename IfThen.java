cb3b4923b5078a2dae3725d35d82ce4279443771
Droid_D3V <79214769+Droid-D3V@users.noreply.github.com>
2022-06-01 23:08:40 -0400

    Create IfThen.java

---
+++ IfThen.java
@@ -0,0 +1,9 @@
+package wtf.droid;
+public class Main {
+    public static void main(String[] args) {
+        String thing = "100";
+        if (thing == "100") {
+            System.out.println("The thing is most definitely a thing.");
+        }
+    }
+}
